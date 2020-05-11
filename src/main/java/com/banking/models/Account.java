d754d4c361756817508b25a85955db535b5f72ab
Scott Slatton <scottslatton.tech@gmail.com>
2020-05-10 18:13:15 -0700

    removed sub accounts

--- src/main/java/com/banking/models/Account.java
+++ src/main/java/com/banking/models/Account.java
@@ -5,15 +5,14 @@ import com.banking.exception.BusinessException;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface Account {
-
-
-
+public class Account {
     double balance = 0.0;
-    String accountType = null;
     List<Customer> customers = new ArrayList<Customer>();
-    void transfer(Customer payer, Customer payee) throws BusinessException;
 
+    public static void transfer(Customer payee) throws BusinessException {
+
+
+    }
 
 
 }
--- src/main/java/com/banking/models/SavingsAccount.java
+++
@@ -1,6 +0,0 @@
-package com.banking.models;
-
-public class SavingsAccount implements Account{
-
-
-}
